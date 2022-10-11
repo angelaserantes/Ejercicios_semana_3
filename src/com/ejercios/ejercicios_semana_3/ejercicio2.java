2da382140bdaf060933461f55642c617a44cad05
angelaserantes <aserantesreboreda@danielcastelao.org>
2022-10-11 21:46:19 +0200

    ejercicio 2

---
+++ src/com/ejercios/ejercicios_semana_3/ejercicio2.java
@@ -0,0 +1,18 @@
+package com.ejercios.ejercicios_semana_3;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        int r = 3;
+        double PI = 3.1415;
+        double a = (PI * (r *r));
+        double l = 2 * PI * r;
+        System.out.println("El área es " +a);
+        System.out.println("La longitud es " +l);
+    }
+}
+
+/* Se hace r * r para poder hacer la potencia el operador ** que viene en los apuntes no lo coge**/
+/* Crea un programa que, dado un valor de radio, calcule el área y la longitud
+de una circunferencia y lo muestre por pantalla.
+Pista: a = pi x r2
+, L = 2 x pi x */
