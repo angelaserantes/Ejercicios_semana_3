042d143dd4690d82af6d7f308fa8e32c950750a3
angelaserantes <aserantesreboreda@danielcastelao.org>
2022-10-11 21:29:56 +0200

    ejercicio 1

--- src/com/ejercios/ejercicios_semana_3/ejercicio1.java
+++ src/com/ejercios/ejercicios_semana_3/ejercicio1.java
@@ -1,4 +1,10 @@
 package com.ejercios.ejercicios_semana_3;
 
 public class ejercicio1 {
+    public static void main(String[] args) {
+    int b = 4;
+    int h = 3;
+    int a = b * h;
+    System.out.println("El área de un rectángulo es " +a);
+    }
 }
