97a332e114fe0bfa213225111ffdff5bdea18b11
angelaserantes <aserantesreboreda@danielcastelao.org>
2022-10-12 18:48:39 +0200

    ejercicio 3

---
+++ src/com/ejercios/ejercicios_semana_3/ejercicio3.java
@@ -0,0 +1,14 @@
+package com.ejercios.ejercicios_semana_3;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+    String name = "Angela";
+    String localidad = "Vigo";
+    String gusto = "me gusta el baloncesto";
+    System.out.println("Hola mi nombre es " +name +"\nSoy de " +localidad +"\n" + gusto);
+    }
+}
+
+/*Crea un programa que, dado un valor fijo de un nombre, una localidad y un
+gusto, los muestre por pantalla
+Pista: “Hola, mi nombre es “ + variable1 + “\nSoy de”+...*/
